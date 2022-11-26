8731c8a19d675f5e5812911b5db89e91d94d6368
Kibet <ronniek625@gmail.com>
2022-11-26 09:09:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback)
+{
+    callback();
+}
+function returnsANamedFunction()
+{
+    function fn()
+    {
+
+    }
+        return fn
+}
+function returnsAnAnonymousFunction()
+{
+
+    return ()=>{}
+}
